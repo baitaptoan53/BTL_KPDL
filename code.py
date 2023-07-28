357116798b769116a5f9b096cb4533653f185c1e
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-07-28 15:00:21 +0700

    first commit

---
+++ code.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+happy_2023 = pd.read_csv("./WHR2023.csv")
+
+
+happy_2023['rank'] = happy_2023['Ladder score'].rank(ascending=False)
+happy_2023['rank'] = happy_2023['rank'].astype(int)
+
+print(happy_2023['rank'])
