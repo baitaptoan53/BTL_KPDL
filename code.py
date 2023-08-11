45f89ea182e82593df9a26229ea337634c63ee90
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-08-11 21:29:45 +0700

    update data

--- code.py
+++ code.py
@@ -5,9 +5,12 @@ import seaborn as sns
 import plotly.express as px
 
 happy_2023 = pd.read_csv("./WHR2023.csv")
+continents2 = pd.read_csv("./continents2.csv")
 
 
-happy_2023['rank'] = happy_2023['Ladder score'].rank(ascending=False)
-happy_2023['rank'] = happy_2023['rank'].astype(int)
+# happy_2023['rank'] = happy_2023['Ladder score'].rank(ascending=False)
+# happy_2023['rank'] = happy_2023['rank'].astype(int)
 
-print(happy_2023['rank'])
+# print(happy_2023['rank'])
+# continents2
+print(continents2)
