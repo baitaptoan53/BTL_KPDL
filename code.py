918647345495d0b9b98863599023793fec836544
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-08-14 18:25:40 +0700

    tien su ly du lieu va phan cum

--- code.py
+++ code.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 from sklearn.cluster import KMeans
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+from tkinter import messagebox
+import webbrowser
+from sklearn.linear_model import LinearRegression
+
+
 happy_2023 = pd.read_csv("./WHR2023.csv")
 country_mapping = pd.read_csv("./continents2.csv")
 # hiển thị số cột và số dòng của bảng happy_2023  và continent_mapping
@@ -62,15 +70,15 @@ country_mapping = country_mapping.drop(
 # Remove all columns between column name 'intermediate-region' to 'intermediate-region-code'
 country_mapping = country_mapping.drop(
     country_mapping.loc[:, 'intermediate-region':'intermediate-region-code'].columns, axis=1)
-country_mapping.head()
+# country_mapping.head()
 
+
+# đổi tên cột trong bảng country_mapping
+country_mapping = country_mapping.rename(
+    {'name': 'quốc gia', 'alpha-3': 'iso alpha', 'sub-region': 'vùng quốc gia', 'region': 'châu lục'}, axis=1)
 # print(country_mapping.head())
 
-# Rename the columns for consistency
-country_mapping = country_mapping.rename(
-    {'name': 'quốc gia', 'alpha-3': 'iso alpha', 'sub-region': 'vùng quốc gia'}, axis=1)
-
-# Merge the happiness data and country mapping dataframes
+# Hợp nhất hai bảng happy_df_2023 và country_mapping thành một bảng mới có tên là happy_region_df
 happy_region_df = happy_df_2023.merge(
     country_mapping, on='quốc gia', how='left')
 # print(happy_region_df.head())
@@ -78,46 +86,181 @@ happy_region_df = happy_df_2023.merge(
 # kiểm tra các giá trị null trong bảng happy_region_df
 # print(happy_region_df.isnull().sum())
 
-# kiểm tra các giá trị null trong cột 'vùng quốc gia'
+
+# kiểm tra các giá trị null trong cột 'châu lục' sau khi merge 2 bảng
+nan_region_rows = happy_region_df[happy_region_df['châu lục'].isnull()]
+# print(nan_region_rows)
+
+# sửa tên quốc gia trong bảng happy_region_df để khớp với bảng country_mapping và bảng happy_2023
+
+# turkey -> turkiye
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Turkey', 'Turkiye', regex=True)
+
+# Palestine, State of ----> State of Palestine
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Palestine, State of', 'State of Palestine', regex=True)
+
+# Côte D'Ivoire ----> Ivory Coast
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    "Côte D'Ivoire", 'Ivory Coast', regex=True)
+
+# Macedonia ----> North Macedonia
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Macedonia', 'North Macedonia', regex=True)
+
+# Hong Kong ----> Hong Kong S.A.R. of China
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Hong Kong', 'Hong Kong S.A.R. of China', regex=True)
+
+# Taiwan ----> Taiwan Province of China
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Taiwan', 'Taiwan Province of China', regex=True)
+
+# Czech Republic ----> Czechia
+country_mapping['quốc gia'] = country_mapping['quốc gia'].str.replace(
+    'Czech Republic', 'Czechia', regex=True)
+
+# Merge lại hai bảng happy_df_2023 và country_mapping thành một bảng mới có tên là happy_region_df
+happy_region_df = happy_df_2023.merge(
+    country_mapping, on='quốc gia', how='left')
+
+# print(happy_region_df.head())
+
+# kiểm tra các giá trị null trong cột 'tuổi thọ'sau khi merge 2 bảng
 nan_hlf_rows = happy_region_df[happy_region_df['tuổi thọ'].isnull(
 )]
 # print(nan_hlf_rows)
+happy_region_df.loc[98, 'tuổi thọ'] = '74.5'
 
-# Check for rows with null region that did not match correctly when merging
-# kiểm tra các giá trị null trong cột 'vùng quốc gia' sau khi merge 2 bảng
-nan_region_rows = happy_region_df[happy_region_df['vùng quốc gia'].isnull()]
-print(nan_region_rows)
+# print(nan_hlf_rows)
+# kiểm tra các giá trị null trong bảng happy_region_df
+nan_region_rows = happy_region_df[happy_region_df['châu lục'].isnull()]
+# print(nan_region_rows)
 
-# happy_2023['rank'] = happy_2023['Ladder score'].rank(ascending=False)
-# happy_2023['rank'] = happy_2023['rank'].astype(int)
+happy_region_df.loc[33, 'châu lục'] = 'Europe'
+happy_region_df.loc[33, 'vùng quốc gia'] = 'Southern Europe'
+happy_region_df.loc[33, 'iso alpha'] = 'XXK'
 
-# # phân cụm theo các nhóm
-# kmeans = KMeans(n_clusters=3, random_state=0).fit(happy_2023[['Ladder score']])
-# happy_2023['cluster'] = kmeans.labels_       # thêm cột cluster vào bảng happy_2023
-# happy_2023['cluster'] = happy_2023['cluster'].astype(int)
+happy_region_df.loc[70, 'châu lục'] = 'Europe'
+happy_region_df.loc[70, 'vùng quốc gia'] = 'Southern Europe'
+happy_region_df.loc[70, 'iso alpha'] = 'BIH'
 
-# # tạo bảng mới gồm các cột: country, cluster, rank, Ladder score
-# happy_2023_new = happy_2023[['Country name', 'cluster', 'rank', 'Ladder score']]
-# happy_2023_new = happy_2023_new.sort_values(by=['rank'], ascending=True)
-# happy_2023_new = happy_2023_new.reset_index(drop=True)
-# # lưu bảng mới vào file csv
-# happy_2023_new.to_csv('happy_2023_new.csv', index=False)
+happy_region_df.loc[85, 'châu lục'] = 'Africa'
+happy_region_df.loc[85, 'vùng quốc gia'] = 'Sub-Saharan Africa'
+happy_region_df.loc[85, 'iso alpha'] = 'COG'
 
-# phân cụm dữ liệu bằng thuật toán kmeans và hiển thị kết quả và biểu đồ
-# kmeans = KMeans(n_clusters=3, random_state=0).fit(happy_2023[['Ladder score']])
-# happy_2023['cluster'] = kmeans.labels_       # thêm cột cluster vào bảng happy_2023
-# happy_2023['cluster'] = happy_2023['cluster'].astype(int)
+happy_region_df.loc[132, 'châu lục'] = 'Africa'
+happy_region_df.loc[132, 'vùng quốc gia'] = 'Sub-Saharan Africa'
+happy_region_df.loc[132, 'iso alpha'] = 'COD'
 
-# tạo bảng mới gồm các cột: country, cluster, rank, Ladder score
-# happy_2023_new = happy_2023[['Country name', 'cluster', 'rank', 'Ladder score']]
-# happy_2023_new = happy_2023_new.sort_values(by=['rank'], ascending=True)
-# happy_2023_new = happy_2023_new.reset_index(drop=True)
-# lưu bảng mới vào file csv
-# happy_2023_new.to_csv('happy_2023_new.csv', index=False)
+# print(happy_region_df)
 
-# vẽ biểu đồ
-# fig = px.scatter(happy_2023_new, x="rank", y="Ladder score", color="cluster",
-#                hover_data=['Country name'])
-# fig.show()
+# happy_region_df.tail(10)
+# print(happy_region_df.isnull().sum())
 
-# xây dựng file csv mapping giữa country và continent
+happy_region_df_3cluster = happy_region_df[['điểm hạnh phúc', 'GDP bình quân đầu người',
+                                            'Hỗ trợ xã hội', 'tự do lựa chọn cuộc sống', 'Tính hào phóng', 'Nhận thức về tham nhũng']]
+# phân cụm dữ liệu trên bằng phương pháp KMeans với số cụm là 3 cụm
+# lưu file happy_region_df_3cluster thành file csv
+happy_region_df_3cluster.to_csv('happy_region_df_3cluster.csv', index=False)
+
+# hiển thị các điểm dữ liệu trên đồ thị với màu sắc tương ứng với cụm mà nó thuộc về
+
+def show_cluster():
+    #phân cụm dùng thuật toán kmeans với số cụm là 3 cụm 
+    kmeans = KMeans(n_clusters=3, random_state=0).fit(happy_region_df_3cluster)
+    #hiển thị đồ thị với màu sắc tương ứng với cụm mà nó thuộc về của các điểm dữ liệu
+    plt.scatter(happy_region_df_3cluster['GDP bình quân đầu người'], happy_region_df_3cluster['điểm hạnh phúc'], c=kmeans.labels_, cmap='rainbow')
+    plt.xlabel('GDP bình quân đầu người')
+    plt.ylabel('điểm hạnh phúc')
+    plt.show()
+
+# xây dựng form nhập liệu để người dùng nhập vào các thông tin cần thiết để dự đoán điểm hạnh phúc của một quốc gia
+
+# khoi tao bien de luu gia tri cua cac bien
+gdp_binh_quan_dau_nguoi = 0
+Ho_tro_xa_hoi = 0
+Tu_do_lua_chon_cuoc_song = 0
+Tinh_hao_phong = 0
+Nhan_thuc_ve_tham_nhuc = 0
+tuoi_tho = 0
+
+
+def input_data():
+    lbl1 = Label(window, text="Nhap GDP bình quân đầu người",
+                 font=("Arial Bold", 10))
+    lbl1.grid(column=0, row=6)
+    txt1 = Entry(window, width=20)
+    txt1.grid(column=1, row=6)
+    lbl2 = Label(window, text="Nhap Hỗ trợ xã hội", font=("Arial Bold", 10))
+    lbl2.grid(column=0, row=7)
+    txt2 = Entry(window, width=20)
+    txt2.grid(column=1, row=7)
+    lbl3 = Label(window, text="Nhap tự do lựa chọn cuộc sống",
+                 font=("Arial Bold", 10))
+    lbl3.grid(column=0, row=8)
+    txt3 = Entry(window, width=20)
+    txt3.grid(column=1, row=8)
+    lbl4 = Label(window, text="Nhap Tính hào phóng", font=("Arial Bold", 10))
+    lbl4.grid(column=0, row=9)
+    txt4 = Entry(window, width=20)
+    txt4.grid(column=1, row=9)
+    lbl5 = Label(window, text="Nhap Nhận thức về tham nhũng",
+                 font=("Arial Bold", 10))
+    lbl5.grid(column=0, row=10)
+    txt5 = Entry(window, width=20)
+    txt5.grid(column=1, row=10)
+    lbl6 = Label(window, text="Nhap tuổi thọ", font=("Arial Bold", 10))
+    lbl6.grid(column=0, row=11)
+    txt6 = Entry(window, width=20)
+    txt6.grid(column=1, row=11)
+    btn5 = Button(window, text="Kết quả", command=ket_qua)
+    btn5.grid(column=1, row=12)
+
+def ket_qua():
+    if (txt1.get() != "" and txt2.get() != "" and txt3.get() != "" and txt4.get() != "" and txt5.get() != "" and txt6.get() != ""):
+        gdp_binh_quan_dau_nguoi = float(txt1.get())
+        Ho_tro_xa_hoi = float(txt2.get())
+        Tu_do_lua_chon_cuoc_song = float(txt3.get())
+        Tinh_hao_phong = float(txt4.get())
+        Nhan_thuc_ve_tham_nhuc = float(txt5.get())
+        tuoi_tho = float(txt6.get())
+        print(gdp_binh_quan_dau_nguoi, Ho_tro_xa_hoi, Tu_do_lua_chon_cuoc_song,Tinh_hao_phong,Nhan_thuc_ve_tham_nhuc,tuoi_tho)
+        # áp dụng mô hình hồi quy tuyến tính để dự đoán điểm hạnh phúc của một quốc gia
+        # đầu vào của mô hình là các thông tin về GDP bình quân đầu người, Hỗ trợ xã hội, tự do lựa chọn cuộc sống, Tính hào phóng, Nhận thức về tham nhũng, tuổi thọ
+        # đầu ra của mô hình là điểm hạnh phúc của một quốc gia
+        # đọc file csv chứa dữ liệu đã được xử lý trước đó
+        df = pd.read_csv('happy_region_df_3cluster.csv')
+        # tạo một mảng chứa các giá trị của các biến đầu vào
+        X = df[['GDP bình quân đầu người', 'Hỗ trợ xã hội', 'Tự do lựa chọn cuộc sống',
+                'Tính hào phóng', 'Nhận thức về tham nhũng', 'tuổi thọ']]
+        # tạo một mảng chứa các giá trị của biến đầu ra
+        y = df['điểm hạnh phúc']
+        # tạo một mô hình hồi quy tuyến tính
+        model = LinearRegression()
+        # đưa dữ liệu vào mô hình để huấn luyện
+        model.fit(X, y)
+        # dự đoán điểm hạnh phúc của một quốc gia
+        y_pred = model.predict([[gdp_binh_quan_dau_nguoi, Ho_tro_xa_hoi,
+            Tu_do_lua_chon_cuoc_song, Tinh_hao_phong, Nhan_thuc_ve_tham_nhuc, tuoi_tho]])
+        lbl7 = Label(window, text="Dự đoán điểm hạnh phúc của một quốc gia là: " +
+            str(y_pred), font=("Arial Bold", 10))
+        lbl7.grid(column=0, row=12)
+
+    else:
+        lbl7 = Label(window, text="Bạn chưa nhập đủ thông tin",
+        font=("Arial Bold", 10))
+        lbl7.grid(column=0, row=12)
+
+
+# tạo form nhập liệu
+window = Tk()
+window.title("Dự đoán điểm hạnh phúc của một quốc gia")
+window.geometry('500x300')
+btn4 = Button(window, text="Nhập dữ liệu", command=input_data)
+btn4.grid(column=0, row=5)
+
+btn6 = Button(window, text="Kết quả phân cụm", command=show_cluster)
+btn6.grid(column=1, row=5)
+window.mainloop()
